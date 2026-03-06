8569540326c2939aba2c2e449e4e1db554b2c3bf
bremmdev <bremmdev@gmail.com>
2026-03-06 19:24:08 +0100

    add singleton DocumentIntelligenceClient

--- functions/Program.cs
+++ functions/Program.cs
@@ -1,3 +1,5 @@
+using Azure;
+using Azure.AI.DocumentIntelligence;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Builder;
 using Microsoft.Extensions.DependencyInjection;
@@ -9,6 +11,25 @@ builder.ConfigureFunctionsWebApplication();
 
 builder.Services
     .AddApplicationInsightsTelemetryWorkerService()
-    .ConfigureFunctionsApplicationInsights();
+    .ConfigureFunctionsApplicationInsights()
+    .AddSingleton(_ =>
+    {
+        string endpoint = Environment.GetEnvironmentVariable(
+            "DOCUMENT_INTELLIGENCE_ENDPOINT"
+        ) ?? throw new InvalidOperationException(
+            "Missing env var: DOCUMENT_INTELLIGENCE_ENDPOINT"
+        );
+
+        string apiKey = Environment.GetEnvironmentVariable(
+            "DOCUMENT_INTELLIGENCE_API_KEY"
+        ) ?? throw new InvalidOperationException(
+            "Missing env var: DOCUMENT_INTELLIGENCE_API_KEY"
+        );
+
+        return new DocumentIntelligenceClient(
+            new Uri(endpoint),
+            new AzureKeyCredential(apiKey)
+        );
+    });
 
 builder.Build().Run();
--- functions/analyzeReceipt.cs
+++ functions/analyzeReceipt.cs
@@ -2,22 +2,35 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
+using Azure.AI.DocumentIntelligence;
+using System.Threading.Tasks;
+using Azure;
 
 namespace ReceiptExtraction.Functions;
 
 public class analyzeReceipt
 {
+    private readonly DocumentIntelligenceClient _client;
     private readonly ILogger<analyzeReceipt> _logger;
 
-    public analyzeReceipt(ILogger<analyzeReceipt> logger)
+    public analyzeReceipt(DocumentIntelligenceClient client, ILogger<analyzeReceipt> logger)
     {
+        _client = client;
         _logger = logger;
     }
 
     [Function("analyzeReceipt")]
-    public IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequest req)
+    public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequest req)
     {
         _logger.LogInformation("C# HTTP trigger function processed a request.");
-        return new OkObjectResult("Welcome to Azure Functions!");
+
+        Operation<AnalyzeResult> operation = await _client.AnalyzeDocumentAsync(
+        WaitUntil.Completed,
+        "prebuilt-receipt",
+        new Uri("https://raw.githubusercontent.com/Azure-Samples/cognitive-services-REST-api-samples/master/curl/form-recognizer/rest-api/receipt.png")
+        );
+        AnalyzeResult result = operation.Value;
+
+        return new OkObjectResult(result);
     }
 }
